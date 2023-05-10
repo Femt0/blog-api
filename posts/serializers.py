c75a31093a3c912062b54a9fa659775ebf709d95
Danila Stelmakh <>
2023-05-10 15:14:55 +0300

    add user authentication, viewsets and routers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
 from .models import Post
@@ -13,3 +15,12 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at',
         )
         model = Post
+        
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = get_user_model()
+        fields = ("id", 'username',)
+    
